c42dcd8e0a8bef086a96be368eca930295b12a7b
yu00212 <yu2000212@icloud.com>
2023-07-03 02:39:55 +0900

    add：Array型

--- Understanding-TS-second-project/app.js
+++ Understanding-TS-second-project/app.js
@@ -1,5 +1,10 @@
 var person = {
     name: 'yota',
     age: 32,
+    hobbies: ['Sports', 'Cooking']
 };
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- Understanding-TS-second-project/app.ts
+++ Understanding-TS-second-project/app.ts
@@ -1,9 +1,11 @@
-const person: {
-    name: string,
-    age: number,
-} = {
+const person = {
     name: 'yota',
     age: 32,
+    hobbies: ['Sports', 'Cooking']
 };
 
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby)
+}
