af7233c65cc141103573a120068ea343de41b876
yu00212 <yu2000212@icloud.com>
2023-07-03 02:33:32 +0900

    add：Object型

--- Understanding-TS-second-project/app.ts
+++ Understanding-TS-second-project/app.ts
@@ -1,15 +1,9 @@
-function add(n1, n2, showResult: boolean, phrase: string) {
-    const result = n1 + n2;
-    if(showResult){
-        console.log(phrase + result);
-    } else {
-        return result;
-    }
-}
+const person: {
+    name: string,
+    age: number,
+} = {
+    name: 'yota',
+    age: 32,
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result: ';
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name)
