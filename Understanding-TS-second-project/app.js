6818f15ce151a0bef91c9b1357c075ffac7af31c
yu00212 <yu2000212@icloud.com>
2023-07-03 02:45:11 +0900

    add：Tuple型

--- Understanding-TS-second-project/app.js
+++ Understanding-TS-second-project/app.js
@@ -1,7 +1,8 @@
 var person = {
     name: 'yota',
     age: 32,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author'],
 };
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
--- Understanding-TS-second-project/app.ts
+++ Understanding-TS-second-project/app.ts
@@ -1,7 +1,13 @@
-const person = {
+const person: {
+    name: string,
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'yota',
     age: 32,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author'],
 };
 
 console.log(person.name)
